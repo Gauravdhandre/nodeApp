2753d6e85b012becc86f500df0f82d4ab65a90b9
Gaurav <gaurav.dhandre@gmail.com>
2018-06-30 16:22:43 +0530

    port added in server.js and start add in package.json

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -70,6 +71,6 @@ app.get('/bad',(request, response) =>{
   });
 });
 
-app.listen(3000, () => {
-	console.log('server is start in 3000 port');
+app.listen(port, () => {
+	console.log(`server is start in 3000 port ${port}`);
 });
